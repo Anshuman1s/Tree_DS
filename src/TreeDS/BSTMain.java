a289131ab4d3b25d7d782580341ca4abab8e83a2
Anshuman <anshumanshukla.12as@gmail.com>
2025-08-05 13:40:43 +0530

    Binary Search Tree

---
+++ src/TreeDS/BSTMain.java
@@ -0,0 +1,11 @@
+package TreeDS;
+
+public class BSTMain {
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+        int[] nums = {5,2,7,1,4,6,9,8,3,10};
+        tree.populate(nums);
+        tree.display();
+        System.out.println(tree.balanced());
+    }
+}
---
+++ src/TreeDS/BinarySearchTree.java
@@ -0,0 +1,75 @@
+package TreeDS;
+import java.util.*;
+public class BinarySearchTree {
+    public class Node{
+        private int data;
+        private int height;
+        private Node left;
+        private Node right;
+        public Node(int data){
+            this.data = data;
+        }
+        public int getData(){
+            return data;
+        }
+    }
+    private Node root;
+    public BinarySearchTree(){
+    }
+    public int height(Node node){
+        if(node == null){
+            return -1;
+        }
+        return node.height;
+    }
+    public boolean isEmpty(){
+        return root == null;
+    }
+//    insert
+    public void insert(int data){
+        root = insert(data, root);
+    }
+    private Node insert(int data, Node node){
+        if(node == null){
+            node = new Node(data);
+            return node;
+        }
+        if(data< node.data){
+            node.left = insert(data,node.left);
+        }
+        if(data > node.data){
+            node.right = insert(data,node.right);
+        }
+        node.height = Math.max(height(node.left), height(node.right) +1);
+        return node;
+    }
+    public void populate(int[] arr){
+        for(int i =0;i<arr.length;i++){
+            this.insert(arr[i]);
+        }
+    }
+    public boolean balanced(){
+        return balanced(root);
+    }
+    private boolean balanced(Node node){
+        if(node == null){
+            return true;
+        }
+        return Math.abs(height(node.left) - height(node.right)) <= 1 && balanced(node.left) && balanced(node.right);
+    }
+    public void display(){
+        display(root,"Root Node: ");
+    }
+    public void display(Node node,String details){
+        if(node == null){
+            return;
+        }
+        System.out.println(details + node.getData());
+        display(node.left , "Left child of " +  node.getData()  + " : ");
+        display(node.right , "Right child of " +  node.getData()  + " : ");
+    }
+
+
+
+
+}
